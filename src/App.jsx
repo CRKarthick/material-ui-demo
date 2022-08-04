705ead0d77e674b9773d9fc96e34cc6de7770bae
CRKarthick <karthick25071998@gmail.com>
2022-08-05 00:35:57 +0530

    Learnt Components and Components API

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,69 @@
+import {
+    AppBar,
+    Button,
+    Card,
+    CardActions,
+    CardContent,
+    CardMedia,
+    Container,
+    CssBaseline,
+    Grid,
+    Toolbar,
+    Typography,
+} from '@mui/material';
 import React from 'react';
+
+import PhotoCameraIcon from '@mui/icons-material/PhotoCamera';
 
 const App = () => {
     return (
-        <div>
-            <h1>Welcome to React</h1>
-        </div>
+        <>
+            <CssBaseline />
+            <AppBar position='relative'>
+                <Toolbar>
+                    <PhotoCameraIcon />
+                    <Typography variant='h6'>Material UI Demo</Typography>
+                </Toolbar>
+            </AppBar>
+            <main>
+                <div>
+                    <Container maxWidth='sm'>
+                        <Typography
+                            variant='h2'
+                            align='center'
+                            color='textPrimary'
+                            gutterBottom
+                        >
+                            Photo Album
+                        </Typography>
+                        <Typography
+                            variant='h6'
+                            align='center'
+                            color='textSecondary'
+                            gutterBottom
+                            paragraph
+                        >
+                            Hello this is Karthick, and I am learning how to use
+                            MUI components on my react application.
+                        </Typography>
+                        <div>
+                            <Grid container spacing={2} justifyContent='center'>
+                                <Grid item>
+                                    <Button variant='contained'>
+                                        Main Call to Action
+                                    </Button>
+                                </Grid>
+                                <Grid item>
+                                    <Button variant='outlined'>
+                                        Secondary Action
+                                    </Button>
+                                </Grid>
+                            </Grid>
+                        </div>
+                    </Container>
+                </div>
+            </main>
+        </>
     );
 };
 
